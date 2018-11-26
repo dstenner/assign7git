19b6352ce117991a3b9a46041f76f86db552d8cb
DAKOTA-PC\Dakota <dstennerson@cox.net>
2018-11-26 10:17:43 -0600

    Created initial setup for design patterns to be created

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Main {
+
+    /**
+     * Main method runs all the deisgn patterns
+     * @param args parameters for executing functions
+     */
+    
+    public static void main(String[] args) {
+        
+    }
+}
